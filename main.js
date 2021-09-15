474294a1a0893cf38ec16e63939831d2142bcb8b
Nita-cookiecloud <80438015+Nita-cookiecloud@users.noreply.github.com>
2021-09-14 18:34:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+Webcam.set({
+
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' +data_uri+'"/>';
+    });
+}
+ 
+console.log('ml5 version' , ml5.version)
+clasifer = ml5.imageClassifer('https://teachablemachine.withgoogle.com/models/wIg0jdH5e/model.json',modelLoaded);
+
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('catured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confident.toFixed(3);
+    }
+}
